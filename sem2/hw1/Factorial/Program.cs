d5e7b333ef2a2aa2004be8adccdf7c1c497d2c18
MihailKulikov <kulikovmihail90@gmail.com>
2020-02-15 21:51:36 +0300

    added task 1.1

---
+++ sem2/hw1/Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{n}! = {Factorial(n)}");
+        }
+    }
+}
