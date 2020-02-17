a04400e8219b79213e90c1b911417d57ac5cca8f
MihailKulikov <kulikovmihail90@gmail.com>
2020-02-17 18:42:27 +0300

    update to .Net Core 3.1, add invitation to enter

--- sem2/hw1.4/SpiralOutput/Program.cs
+++ sem2/hw1.4/SpiralOutput/Program.cs
@@ -4,41 +4,41 @@ namespace SpiralOutput
 {
     class Program
     {
-        static void SpiralOutput(int[,] matrix)
+        private static void SpiralOutput(int[,] matrix)
         {
             int x = matrix.GetLength(0) / 2;
-            int y = x;
+            var y = x;
             Console.Write($"{matrix[x, y]} ");
-            int shift = 0;
+            var shift = 0;
 
             while (x != matrix.GetLength(0) - 1)
             {
                 shift++;
-                for (int i = 0; i < shift; ++i)
+                for (var i = 0; i < shift; ++i)
                 {
                     y++;
                     Console.Write($"{matrix[x, y]} ");
                 }
-                for (int i = 0; i < shift; ++i)
+                for (var i = 0; i < shift; ++i)
                 {
                     x--;
                     Console.Write($"{matrix[x, y]} ");
                 }
 
                 shift++;
-                for (int i = 0; i < shift; ++i)
+                for (var i = 0; i < shift; ++i)
                 {
                     y--;
                     Console.Write($"{matrix[x, y]} ");
                 }
-                for (int i = 0; i < shift; ++i)
+                for (var i = 0; i < shift; ++i)
                 {
                     x++;
                     Console.Write($"{matrix[x, y]} ");
                 }
             }
 
-            for (int i = 1; i < matrix.GetLength(1); ++i)
+            for (var i = 1; i < matrix.GetLength(1); ++i)
             {
                 y++;
                 Console.Write($"{matrix[x, y]} ");
@@ -47,15 +47,16 @@ namespace SpiralOutput
 
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            string[] variables = input.Split(new char[] { ' ' });
-            int N = int.Parse(variables[0]);
-            int[,] matrix = new int[N, N];
-            int index = 1;
+            Console.WriteLine("Enter the size of the matrix:");
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter elements of the matrix separated by space:");
+            var variables = Console.ReadLine().Split(new char[] { ' ' });
+            var matrix = new int[size, size];
+            var index = 0;
 
-            for (int i = 0; i < matrix.GetLength(0); ++i)
+            for (var i = 0; i < matrix.GetLength(0); ++i)
             {
-                for (int j = 0; j < matrix.GetLength(1); ++j)
+                for (var j = 0; j < matrix.GetLength(1); ++j)
                 {
                     matrix[i, j] = int.Parse(variables[index]);
                     index++;
