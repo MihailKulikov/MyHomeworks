354774a1ddda395b9212e81fd7c5e474f18d6fc7
MihailKulikov <kulikovmihail90@gmail.com>
2020-02-18 12:30:41 +0300

    Added Shell sort

--- sem2/hw1.3/ArraySort/Program.cs
+++ sem2/hw1.3/ArraySort/Program.cs
@@ -1,23 +1,44 @@
 ﻿using System;
+using System.Linq;
 
 namespace ArraySort
 {
     class Program
     {
+        private static void Swap(ref int a, ref int b)
+        {
+            a += b;
+            b = a - b;
+            a = a - b;
+        }
+
+        private static void ShellSort(ref int[] array)
+        {
+            var step = array.Length / 2;
+            while (step >= 1)
+            {
+                for (var i = step; i < array.Length; i++)
+                {
+                    var j = i;
+                    while ((j >= step) && (array[j - step] > array[j]))
+                    {
+                        Swap(ref array[j], ref array[j - step]);
+                        j = j - step;
+                    }
+                }
+
+                step = step / 2;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the size of the array:");
-            int size = int.Parse(Console.ReadLine());
-            var buffer = new int[size];
-            Console.WriteLine("Enter elements of the array:");
-            for (var i = 0; i < buffer.Length; ++i)
-            {
-                buffer[i] = int.Parse(Console.ReadLine());
-            }
-            Array.Sort(buffer);
-            for (var i = 0; i < buffer.Length; ++i)
-            {
-                Console.Write($"{buffer[i]} ");
+            Console.WriteLine("Enter elements of the array separated by space:");
+            var buffer = Console.ReadLine().Split(new char[] { ' ' }).Select(int.Parse).ToArray() ;
+            ShellSort(ref buffer);
+            foreach (var i in buffer)
+            {   
+                Console.Write($"{i} ");
             }
         }
     }
