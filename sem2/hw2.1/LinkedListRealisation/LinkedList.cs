02fd1ab9beeb673937302dffee6c96cfc6657b59
MihailKulikov <kulikovmihail90@gmail.com>
2020-03-01 20:34:13 +0300

    added 2.1 task

---
+++ sem2/hw2.1/LinkedListRealisation/LinkedList.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace LinkedListRealisation
+{
+    public class LinkedList<T>
+    {
+        private class Node
+        {
+            public T Value { get; set; }
+
+            public Node Next { get; set; }
+
+            public Node(T value) => Value = value;
+
+            public Node(T value, Node next)
+            {
+                Value = value;
+                Next = next;
+            }
+        }
+
+        private Node Head { get; set; }
+        private Node Tail { get; set; }
+        public int Length { get; private set; }
+
+        public bool IsEmpty()
+            => Head == null;
+
+        private Node FindNodeByIndex(int index)
+        {
+            var current = Head;
+            for (var i = 0; i < index; i++)
+            {
+                current = current.Next ?? throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            return current;
+        }
+
+        public void AddElementByIndex(T value, int index)
+        {
+            if ((index > Length) || (index < 0))
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (IsEmpty())
+            {
+                Head = new Node(value);
+                Tail = Head;
+            }
+            else
+            {
+                if (index == 0)
+                {
+                    Head = new Node(value, Head);
+                }
+                else if (index == Length)
+                {
+                    Tail.Next = new Node(value);
+                    Tail = Tail.Next;
+                }
+                else
+                {
+                    var previous = FindNodeByIndex(index - 1);
+                    var current = new Node(value, previous.Next);
+                    previous.Next = current;
+                }
+            }
+
+            Length++;
+        }
+
+        public void RemoveElementByIndex(int index)
+        {
+            if ((index >= Length) || (index < 0))
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            if (index == 0)
+            {
+                Head = Head.Next;
+            }
+            else
+            {
+                var previous = FindNodeByIndex(index - 1);
+                if (previous.Next.Next == null)
+                {
+                    Tail = previous;
+                    Tail.Next = null;
+                }
+                else
+                {
+                    previous.Next = previous.Next.Next;
+                }
+            }
+
+            if (IsEmpty()) Tail = null;
+            Length--;
+        }
+
+        public T GetValueByIndex(int index)
+        {
+            if ((index >= Length) || (index < 0))
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            return FindNodeByIndex(index).Value;
+        }
+
+        public void SetValueByIndex(T value, int index)
+        {
+            if ((index >= Length) || (index < 0))
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            FindNodeByIndex(index).Value = value;
+        }
+
+        public void PrintList()
+        {
+            var current = Head;
+            while (current != null)
+            {
+                Console.Write($"{current.Value} ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ sem2/hw2.1/LinkedListRealisation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LinkedListRealisation
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var list = new LinkedList<int>();
+            list.AddElementByIndex(12, 0);
+            list.AddElementByIndex(45, 1);
+            list.AddElementByIndex(34, 1);
+            Console.WriteLine(list.Length);
+            list.AddElementByIndex(14, 0);
+            list.RemoveElementByIndex(0);
+            list.PrintList();
+            list.RemoveElementByIndex(list.Length - 1);
+            Console.WriteLine(list.GetValueByIndex(1));
+            list.SetValueByIndex(1,1);
+            list.PrintList();
+        }
+    }
+}
