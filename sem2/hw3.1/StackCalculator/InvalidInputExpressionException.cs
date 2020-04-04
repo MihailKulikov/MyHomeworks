8cb0c73a8724ed4570442fd07006b454cc77a15b
MihailKulikov <kulikovmihail90@gmail.com>
2020-04-05 01:48:40 +0300

    changed InvalidInputExpression

--- sem2/hw3.1/StackCalculator/InvalidInputExpressionException.cs
+++ sem2/hw3.1/StackCalculator/InvalidInputExpressionException.cs
@@ -2,20 +2,18 @@
 
 namespace StackCalculator
 {
+    [Serializable]
     public class InvalidInputExpressionException : Exception
     {
-        public InvalidInputExpressionException()
-        {
-        }
+        public InvalidInputExpressionException() { }
 
         public InvalidInputExpressionException(string message)
-            : base(message)
-        {
-        }
+            : base(message) { }
 
         public InvalidInputExpressionException(string message, Exception inner)
-            : base(message, inner)
-        {
-        }
+            : base(message, inner) { }
+
+        protected InvalidInputExpressionException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
