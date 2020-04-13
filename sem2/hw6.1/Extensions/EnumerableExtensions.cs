cec3f1aab5ba6922e2e1b5a342780960d97c1a23
MihailKulikov <kulikovmihail90@gmail.com>
2020-04-13 19:30:08 +0300

    added all functionality

---
+++ sem2/hw6.1/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<TResult> Map<TSource,TResult>(this IEnumerable<TSource> inputEnumerable, Func<TSource, TResult> changer)
+        {
+            foreach (var item in inputEnumerable)
+            {
+                yield return changer(item);
+            }
+        }
+
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> inputEnumerable, Func<T, bool> filter)
+        {
+            foreach (var item in inputEnumerable)
+            {
+                if (filter(item))
+                    yield return item;
+            }
+        }
+
+        public static TResult Fold<TSource, TResult>(this IEnumerable<TSource> inputEnumerable, TResult seed,
+            Func<TResult, TSource, TResult> func)
+        {
+            foreach (var item in inputEnumerable)
+            {
+                seed = func(seed, item);
+            }
+
+            return seed;
+        }
+    }
+}
---
+++ sem2/hw6.1/Extensions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new List<string> {"I", "am", "here"};
+
+            var result = test.Fold(0, (seed, item) => seed + item.Length);
+            
+            Console.WriteLine(result);
+        }
+    }
+}
