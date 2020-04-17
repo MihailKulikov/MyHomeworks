91c4ce9d844f5a4925d0ce4af2e54ed2a244a2a3
MihailKulikov <kulikovmihail90@gmail.com>
2020-04-17 16:57:24 +0300

    added some test

---
+++ sem2/Queue/QueueTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace QueueTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
